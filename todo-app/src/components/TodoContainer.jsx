254cc6939665e7b7a1d80c9174c2d78beea493ce
TimiMaliki <malikitimi@gmail.com>
2023-11-17 15:11:16 +0100

    added: a todo-App Project

---
+++ todo-app/src/components/TodoContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoContainer = () => {
+  return (
+    <div className='container mx-auto text-center mt-20 bg-gray-200 w-full h-[500px] p-20'>
+      <h1 className='-mt-2 text-2xl font-bold'>Todo App</h1>
+    </div>
+  )
+}
+
+export default TodoContainer
